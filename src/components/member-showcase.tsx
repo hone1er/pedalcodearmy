6cf7ccc78edb497648b299a4be94207424b4c2ff
hone1er <24376928+hone1er@users.noreply.github.com>
2024-12-22 15:30:50 -0800

    add setup view

--- src/components/member-showcase.tsx
+++ src/components/member-showcase.tsx
@@ -1,7 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { BikeIcon as Motorcycle, Instagram, Youtube } from "lucide-react";
+import {
+  BikeIcon as Motorcycle,
+  Instagram,
+  Youtube,
+  ArrowLeft,
+  ArrowRight,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -14,22 +20,49 @@ const members = [
       {
         name: "Hobbit PA 50II",
         image: "/images/honesMopeds/hobbit.JPG",
+        setup: {
+          kit: "Stocko shocko",
+          carb: "Mikuni VM20",
+          pipe: "Proma",
+          variator: "Treats Prototype",
+        },
       },
       {
         name: "Magnum II",
         image: "/images/honesMopeds/magnum.JPG",
+        setup: {
+          kit: "puch treat kit party - 70cc TCCD kit / CUSTOM cut head",
+          carb: "Dellorto 15.15 SHA",
+          pipe: "Black Pipe",
+        },
       },
       {
         name: "Derbi Variant TT",
         image: "/images/honesMopeds/derbi.JPG",
+        setup: {
+          kit: "stock",
+          carb: "Dellorto 15.15 SHA",
+          pipe: "Tecno Boss",
+          variator: "Stock",
+        },
       },
       {
         name: "Maxi N",
         image: "/images/honesMopeds/maxi_n.JPG",
+        setup: {
+          kit: "Mystery Kit",
+          carb: "Dellorto 15.15 SHA",
+          pipe: "Tecno Boss",
+        },
       },
       {
         name: "Little Yellow Maxi",
         image: "/images/honesMopeds/maxi.JPG",
+        setup: {
+          kit: "Stock",
+          carb: "Dellorto 14 SHA",
+          pipe: "Tecno Boss",
+        },
       },
     ],
     instagram: "hone1er",
@@ -94,7 +127,9 @@ export default function MemberShowcase() {
   const [selectedMoped, setSelectedMoped] = useState<{
     name: string;
     image: string;
+    setup?: Record<string, string | undefined>;
   } | null>(null);
+  const [showSetup, setShowSetup] = useState(false);
   return (
     <div className="container mx-auto px-4 py-12">
       <h1 className="mb-12 text-center text-4xl font-bold md:text-5xl">
@@ -153,7 +188,10 @@ export default function MemberShowcase() {
                               </h2>
                               <div
                                 className="relative cursor-pointer pb-[75%]"
-                                onClick={() => setSelectedMoped(moped)}
+                                onClick={() => {
+                                  setShowSetup(false);
+                                  setSelectedMoped(moped);
+                                }}
                               >
                                 <Image
                                   src={moped.image}
@@ -165,20 +203,102 @@ export default function MemberShowcase() {
                               </div>
                             </div>
                           </DialogTrigger>
-                          <DialogContent className="px-2">
-                            <DialogHeader>
-                              <DialogTitle>{moped.name}</DialogTitle>
-                              <div className="relative h-[75vh] w-full">
-                                <Image
-                                  src={moped.image}
-                                  alt={moped.name}
-                                  layout="fill"
-                                  objectFit="contain"
-                                  objectPosition="center"
-                                />
-                              </div>
-                            </DialogHeader>
-                          </DialogContent>
+                          {selectedMoped && (
+                            <DialogContent className="px-2">
+                              {!showSetup ? (
+                                <DialogHeader>
+                                  <DialogTitle>
+                                    {selectedMoped.name}
+                                  </DialogTitle>
+                                  <div className="relative h-[75vh] w-full">
+                                    <Button
+                                      variant={"ghost"}
+                                      className="absolute left-2 top-[50%] z-10"
+                                      onClick={() => {
+                                        const currentIndex =
+                                          member.mopeds.findIndex(
+                                            (m) =>
+                                              m.name === selectedMoped.name,
+                                          );
+                                        const newIndex =
+                                          currentIndex === 0
+                                            ? member.mopeds.length - 1
+                                            : currentIndex - 1;
+                                        const newMoped =
+                                          member.mopeds[newIndex];
+                                        if (newMoped)
+                                          setSelectedMoped(newMoped);
+                                      }}
+                                    >
+                                      <ArrowLeft className="text-white" />
+                                    </Button>
+                                    <Image
+                                      src={selectedMoped.image}
+                                      alt={selectedMoped.name}
+                                      layout="fill"
+                                      objectFit="contain"
+                                      objectPosition="center"
+                                    />
+                                    <Button
+                                      variant={"ghost"}
+                                      className="absolute right-2 top-[50%] z-10"
+                                      onClick={() => {
+                                        const currentIndex =
+                                          member.mopeds.findIndex(
+                                            (m) =>
+                                              m.name === selectedMoped.name,
+                                          );
+                                        const newIndex =
+                                          currentIndex ===
+                                          member.mopeds.length - 1
+                                            ? 0
+                                            : currentIndex + 1;
+                                        const newMoped =
+                                          member.mopeds[newIndex];
+                                        if (newMoped)
+                                          setSelectedMoped(newMoped);
+                                      }}
+                                    >
+                                      <ArrowRight className="text-white" />
+                                    </Button>
+                                  </div>
+                                </DialogHeader>
+                              ) : (
+                                <DialogHeader>
+                                  <DialogTitle className="pb-4">
+                                    {selectedMoped.name}&apos;s setup
+                                  </DialogTitle>
+                                  <div>
+                                    {selectedMoped.setup && (
+                                      <div className="grid grid-cols-2 gap-4">
+                                        {Object.entries(
+                                          selectedMoped.setup,
+                                        ).map(([key, value]) => (
+                                          <div key={key}>
+                                            <h4 className="font-semibold">
+                                              {key}
+                                            </h4>
+                                            <p>{value}</p>
+                                          </div>
+                                        ))}
+                                      </div>
+                                    )}
+                                  </div>
+                                </DialogHeader>
+                              )}
+                              <DialogFooter>
+                                {selectedMoped.setup && (
+                                  <Button
+                                    onClick={() => {
+                                      setShowSetup((prev) => !prev);
+                                    }}
+                                  >
+                                    {showSetup ? "Back" : "Show Setup"}
+                                  </Button>
+                                )}
+                              </DialogFooter>
+                            </DialogContent>
+                          )}
                         </Dialog>
                       </div>
                     ))}
@@ -197,6 +317,7 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
